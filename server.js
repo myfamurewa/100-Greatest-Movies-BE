a7d6e95ef9b9353db15f966838eef263677c3b5a
Michael Famurewa <myfamurewa@gmail.com>
2020-10-01 17:35:14 -0400

    fixing server

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require('dotenv').config()
 const express = require("express");
+const passport = require('passport')
+const morgan = require('morgan')
 const helmet = require("helmet");
 const cors = require("cors");
-
+const server = express
 server.use(helmet.noSniff()); 
 server.use(express.json());
 server.use(cors());
+server.use(morgan('dev'))
+server.use(passport.initialize())
+server.use(passport.session())
+const authRouter = require('./routers/auth-router')
+const commentRouter = require('./routers/comments-router')
+const moviesRouter = require('./routers/movies-router')
+const userRouter = require('./routers/users-router')
 
-
+server.use('/auth', authRouter)
+server.use('/users', userRouter)
+server.use('/movies', moviesRouter)
+server.use('/comments', commentRouter)
 server.get("/", (req, res) => {
     res.status(200).send("API functional")
 })
